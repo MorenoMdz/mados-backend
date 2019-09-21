df1463737bd8e2cf3a8d959b8aa6effabe88d4df
Moreno Antunes <morenomdz@gmail.com>
2019-09-21 15:32:38 -0300

    Vai faltar luz

--- app/Controllers/Http/ServiceOrderController.js
+++ app/Controllers/Http/ServiceOrderController.js
@@ -7,6 +7,9 @@
 /**
  * Resourceful controller for interacting with serviceorders
  */
+
+const ServiceOrder = use('App/Models/ServiceOrder')
+
 class ServiceOrderController {
   /**
    * Show a list of all serviceorders.
@@ -18,18 +21,8 @@ class ServiceOrderController {
    * @param {View} ctx.view
    */
   async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new serviceorder.
-   * GET serviceorders/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    const so = await ServiceOrder.query().with('address').with('user').fetch()
+    return so
   }
 
   /**
@@ -40,7 +33,75 @@ class ServiceOrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request, auth }) {
+    const data = request.only(['creator_id',
+      'client_id',
+      'store_id',
+      'equipment_id',
+      'serial_number',
+      'equipment_model',
+      'accessories',
+      'os_number',
+      'os_type',
+      'os_status_id',
+      'diag_status_id',
+      'problem_description',
+      'repair_status_id',
+      'observation',
+      'payment_status_id',
+      'payment_type',
+      'paid_value',
+      'delivery_date',
+      'warranty',
+      'received_by'])
+    data.active = true
+    data.creator_id = auth.user ? auth.user.id : 1
+    const serviceOrder = await ServiceOrder.create(data)
+
+    // const query = {
+    //   "creator_id": 1,
+    //   "client_id": 1,
+    //   "store_id": 1,
+    //   "equipment_id": 1,
+    //   "serial_number": "abc123",
+    //   "equipment_model": "abc",
+    //   "accessories": "abc",
+    //   "os_number": "A000123",
+    //   "os_type": "type 1",
+    //   "os_status_id": 1,
+    //   "diag_status_id": 1,
+    //   "problem_description": "problem description 1'",
+    //   "repair_status_id": 1,
+    //   "observation": "os observation 1",
+    //   "payment_status_id": 1,
+    //   "payment_type": "type 1",
+    //   "paid_value": 123,
+    //   "delivery_date": "11/11/2011",
+    //   "warranty": "90 days",
+    //   "received_by": "john doe",
+
+    //   "creator_id",
+    //   "client_id",
+    //   "store_id",
+    //   "equipment_id",
+    //   "serial_number",
+    //   "equipment_model",
+    //   "accessories",
+    //   "os_number",
+    //   "os_type",
+    //   "os_status_id",
+    //   "diag_status_id",
+    //   "problem_description",
+    //   "repair_status_id",
+    //   "observation",
+    //   "payment_status_id",
+    //   "payment_type",
+    //   "paid_value",
+    //   "delivery_date",
+    //   "warranty",
+    //   "received_by",
+    // }
+    return serviceOrder
   }
 
   /**
